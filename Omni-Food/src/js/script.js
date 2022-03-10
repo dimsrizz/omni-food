9ac27d86b96f46aba3d6bbc5fc1940d5e88cc544
Dim4SR <92663737+Dim4SR@users.noreply.github.com>
2022-03-10 12:34:43 +0700

    Add files via upload

---
+++ Omni-Food/src/js/script.js
@@ -0,0 +1,53 @@
+const headerEl = document.querySelector(".header");
+const btnNav = document.querySelector(".btn-mobile-nav");
+
+btnNav.addEventListener("click", function (e) {
+  e.preventDefault();
+  headerEl.classList.toggle("nav-open");
+});
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    const href = link.getAttribute("href");
+    e.preventDefault();
+    console.log(href);
+
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
